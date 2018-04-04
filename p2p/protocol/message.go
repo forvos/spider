94028700e95296c56cc8cd94e6ed0c481022becf
yanlong <592938410@qq.com>
2018-04-04 19:21:25 +0800

    Adjust file

--- p2p/protocol/message.go
+++ p2p/protocol/message.go
@@ -1,10 +1,5 @@
 package protocol
 
-import (
-	"encoding/binary"
-	"errors"
-)
-
 // Head protocol head 8+16+8+8+64+64 = 168 bit len
 // 1+2+1+1+8+8 = 21 byte
 type Head struct {
@@ -54,53 +49,4 @@ func NewMessage(head Head, payLoad []byte) *Message {
 		head:    head,
 		PayLoad: payLoad,
 	}
-}
-
-type Protocol struct{}
-
-func (p *Protocol) DeCode(data []byte) (*Message, error) {
-	head := Head{}
-	if len(data) < 21 {
-		return nil, errors.New("length too small")
-	}
-
-	b := binary.BigEndian
-	head.version = data[0]
-	if uint(head.version) != version {
-		return nil, errors.New("protocol version error")
-	}
-
-	head.magic = b.Uint16(data[1:3])
-	if head.magic != magic {
-		return nil, errors.New("magic error")
-	}
-
-	head.msgType = data[3]
-	head.gz = data[4]
-	head.reqId = b.Uint64(data[5:13])
-	head.len = b.Uint64(data[13:21])
-
-	if uint64(len(data)) < 21+head.len {
-		return nil, errors.New("length too small")
-	}
-
-	return &Message{
-		head:    head,
-		PayLoad: data[21 : 21+head.len],
-	}, nil
-}
-
-func (p *Protocol) EnCode(msg *Message) ([]byte, error) {
-	var (
-		b    = binary.BigEndian
-		data = make([]byte, 21)
-	)
-	data[0] = version
-	b.PutUint16(data[1:3], magic)
-	data[3] = msg.head.msgType
-	data[4] = msg.head.gz
-	b.PutUint64(data[5:13], msg.head.reqId)
-	b.PutUint64(data[13:], msg.head.len)
-	data = append(data, msg.PayLoad...)
-	return data, nil
 }
--- p2p/protocol/protocol.go
+++ p2p/protocol/protocol.go
@@ -1,5 +1,10 @@
 package protocol
 
+import (
+	"encoding/binary"
+	"errors"
+)
+
 const version = 0x01
 const magic = 0x594C
 
@@ -7,3 +12,52 @@ const (
 	NO  = 0x00
 	ZIP = 0x01
 )
+
+type Protocol struct{}
+
+func (p *Protocol) DeCode(data []byte) (*Message, error) {
+	head := Head{}
+	if len(data) < 21 {
+		return nil, errors.New("length too small")
+	}
+
+	b := binary.BigEndian
+	head.version = data[0]
+	if uint(head.version) != version {
+		return nil, errors.New("protocol version error")
+	}
+
+	head.magic = b.Uint16(data[1:3])
+	if head.magic != magic {
+		return nil, errors.New("magic error")
+	}
+
+	head.msgType = data[3]
+	head.gz = data[4]
+	head.reqId = b.Uint64(data[5:13])
+	head.len = b.Uint64(data[13:21])
+
+	if uint64(len(data)) < 21+head.len {
+		return nil, errors.New("length too small")
+	}
+
+	return &Message{
+		head:    head,
+		PayLoad: data[21 : 21+head.len],
+	}, nil
+}
+
+func (p *Protocol) EnCode(msg *Message) ([]byte, error) {
+	var (
+		b    = binary.BigEndian
+		data = make([]byte, 21)
+	)
+	data[0] = version
+	b.PutUint16(data[1:3], magic)
+	data[3] = msg.head.msgType
+	data[4] = msg.head.gz
+	b.PutUint64(data[5:13], msg.head.reqId)
+	b.PutUint64(data[13:], msg.head.len)
+	data = append(data, msg.PayLoad...)
+	return data, nil
+}
